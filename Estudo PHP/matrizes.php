a99da5221ecdfcb6cbfb641aae3742667c106672
José Carlos <jcps.filho@gmail.com>
2018-08-04 12:17:05 -0300

    Envio da aula de matrizes

---
+++ Estudo PHP/matrizes.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Matrizes</title>
+</head>
+<body>
+    <?php
+    
+        $tabulerio[8]['a'] = 'torre preta';
+        $tabulerio[8]['b'] = 'bispo preta';
+        $tabulerio[8]['c'] = 'cavalo preta';
+        $tabulerio[8]['d'] = 'rainha preta';
+        $tabulerio[8]['e'] = 'rei preta';
+        $tabulerio[8]['f'] = 'bispo preta';
+        $tabulerio[8]['g'] = 'cavalo preta';
+        $tabulerio[8]['h'] = 'torre preta';
+
+        $tabulerio[7]['a'] = 'peão preto';
+        $tabulerio[7]['b'] = 'peão preto';
+        $tabulerio[7]['c'] = 'peão preto';
+        $tabulerio[7]['d'] = 'peão preto';
+        $tabulerio[7]['e'] = 'peão preto';
+        $tabulerio[7]['f'] = 'peão preto';
+        $tabulerio[7]['g'] = 'peão preto';
+        $tabulerio[7]['h'] = 'peão preto';
+
+        $tabulerio[6]['a'];
+        $tabulerio[6]['b'];
+        $tabulerio[6]['c'];
+        $tabulerio[6]['d'];
+        $tabulerio[6]['e'];
+        $tabulerio[6]['f'];
+        $tabulerio[6]['g'];
+        $tabulerio[6]['h'];
+
+        $tabulerio[5]['a'];
+        $tabulerio[5]['b'];
+        $tabulerio[5]['c'];
+        $tabulerio[5]['d'];
+        $tabulerio[5]['e'];
+        $tabulerio[5]['f'];
+        $tabulerio[5]['g'];
+        $tabulerio[5]['h'];
+
+        $tabulerio[4]['a'];
+        $tabulerio[4]['b'];
+        $tabulerio[4]['c'];
+        $tabulerio[4]['d'];
+        $tabulerio[4]['e'];
+        $tabulerio[4]['f'];
+        $tabulerio[4]['g'];
+        $tabulerio[4]['h'];
+        
+        $tabulerio[3]['a'];
+        $tabulerio[3]['b'];
+        $tabulerio[3]['c'];
+        $tabulerio[3]['d'];
+        $tabulerio[3]['e'];
+        $tabulerio[3]['f'];
+        $tabulerio[3]['g'];
+        $tabulerio[3]['h'];
+
+        $tabulerio[2]['a'] = 'peão branco';
+        $tabulerio[2]['b'] = 'peão branco';
+        $tabulerio[2]['c'] = 'peão branco';
+        $tabulerio[2]['d'] = 'peão branco';
+        $tabulerio[2]['e'] = 'peão branco';
+        $tabulerio[2]['f'] = 'peão branco';
+        $tabulerio[2]['g'] = 'peão branco';
+        $tabulerio[2]['h'] = 'peão branco';
+
+        $tabulerio[1]['a'] = 'torre branca';
+        $tabulerio[1]['b'] = 'cavalo branco';
+        $tabulerio[1]['c'] = 'bispo branco';
+        $tabulerio[1]['d'] = 'rainha branca';
+        $tabulerio[1]['e'] = 'rei branco';
+        $tabulerio[1]['f'] = 'bispo branco';
+        $tabulerio[1]['g'] = 'cavalo branco';
+        $tabulerio[1]['h'] = 'torre branca';
+
+        var_dump($tabulerio);
+    
+    ?>
+</body>
+</html>
