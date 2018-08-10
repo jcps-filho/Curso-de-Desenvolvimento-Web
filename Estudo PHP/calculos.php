a7fc2670fa2997d573be4bf22ad9f6fd776ae3e2
José Carlos <jcps.filho@gmail.com>
2018-08-10 10:51:37 -0300

    Envio da aula de calculos

---
+++ Estudo PHP/calculos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculos</title>
+</head>
+<body>
+    <?php
+    
+        // + adição;
+        // - subtração;
+        // * multiplicação;
+        // / divisão;
+        // % modulo;
+
+        $n1 = 7;
+        $n2 = 3;
+
+        //adição
+        echo $n1 + $n2;
+        echo '<br>';
+        //subtração
+        echo $n1 - $n2;
+        echo '<br>';
+        //multiplicação
+        echo $n1 * $n2;
+        echo '<br>';
+        //divisão
+        echo $n1 / $n2;
+        echo '<br>';
+        //modulo
+        echo $n1 % $n2;
+        echo '<br>';
+
+    
+    ?>
+</body>
+</html>
